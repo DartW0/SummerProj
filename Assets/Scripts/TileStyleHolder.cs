45b81e09613daf234a7c7a8ee1e25060e2ce3a7c
DartW0 <kori789@mail.ru>
2016-03-16 02:46:17 +0300

    Added managers (game, input, tile style)

---
+++ Assets/Scripts/TileStyleHolder.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class TileStyle
+{
+    public int number;
+    public Color32 TileColor;
+    public Color32 TextColor;
+}
+
+public class TileStyleHolder : MonoBehaviour {
+
+    public static TileStyleHolder Instance;
+
+    public TileStyle[] TileStyles;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+}
